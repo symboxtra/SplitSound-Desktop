5b5543aafa874b7b449e1457e405616c7a523bde
Neel Patel <patelneel55@gmail.com>
2018-04-20 12:08:08 -0400

    Add QTDIR path and redirect pipefail

--- test.ps1
+++ test.ps1
@@ -6,7 +6,21 @@
 
 ""
 # Create build folders
-mkdir build
-cd build
+rm -r build > $null 2>&1
+mkdir build > $null
+cd build > $null
+
+# Generate and build CMake
+
+Write-Host "===== Building CMake... =====" -Foreground Yellow
+""
+
+# Build CMake for 64-bit arch 
 cmake .. -A x64
+if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
+
+# Generate executable using CMake
+Write-Host "===== Building CMake... =====" -Foreground Yellow
+""
 cmake --build .
+if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
