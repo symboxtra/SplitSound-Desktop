9d8c697ff4c0f801cea73ccacac1899ce8ff98e3
Neel Patel <patelneel55@gmail.com>
2018-04-20 11:34:09 -0400

    Configure AppVeyor

---
+++ test.ps1
@@ -0,0 +1,12 @@
+#########################################################
+#                                                       #
+#		Build QT for Windows using MSVC compiler		#
+#                                                       #
+#########################################################
+
+""
+# Create build folders
+mkdir build
+cd build
+cmake .. -A x64
+cmake --build ./build
