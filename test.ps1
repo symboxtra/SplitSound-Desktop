deeb3271f1b9dfe6db74a0d1f151084a6801f3fe
Neel Patel <patelneel55@gmail.com>
2018-04-23 14:22:46 -0400

    Remove CMake from test script

--- test.ps1
+++ test.ps1
@@ -4,24 +4,9 @@
 #                                                       #
 #########################################################
 
-""
-# Create build folders
-rm -r build > $null 2>&1
-mkdir build > $null
-cd build > $null
-
-# Generate and build CMake
-
-Write-Host "===== Building CMake... =====" -Foreground Yellow
-""
-
-# Build CMake for 64-bit arch 
-cmake .. -A x64
-if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
-
 # Generate executable using CMake
 ""
-Write-Host "===== Building Executable... =====" -Foreground Yellow
+Write-Host "===== Building... =====" -Foreground Yellow
 ""
 cmake --build .
 if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
