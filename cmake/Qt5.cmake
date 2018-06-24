26de02bc5a9231684781c2b15222fda81156b1e3
Neel Patel <patelneel55@gmail.com>
2018-06-24 11:56:17 -0500

    Merge cmake/ from material-ui

---
+++ cmake/Qt5.cmake
@@ -0,0 +1,48 @@
+# Macro to call windeployqt after building
+macro (qt5_post_build_windeployqt APP QMLDIR)
+
+    # Get bin directory of Qt from Qt5Core.dll
+    find_package(Qt5 REQUIRED Core)
+    get_target_property(Qt5_CoreLocation Qt5::Core LOCATION)
+    get_filename_component(QTBIN ${Qt5_CoreLocation} DIRECTORY)
+    MESSAGE(STATUS "\nQt bin: " ${QTBIN} "\n")
+    
+    # Just copy manually to avoid the trouble
+    add_custom_command(TARGET ${APP} POST_BUILD
+       COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<CONFIG:Debug>:"${QTBIN}/Qt5QuickTemplates2d.dll"> $<$<NOT:$<CONFIG:Debug>>:"${QTBIN}/Qt5QuickTemplates2.dll"> $<TARGET_FILE_DIR:${APP}>)
+    
+    add_custom_command(TARGET ${APP} POST_BUILD
+            COMMAND "${QTBIN}/windeployqt.exe" -qmldir ${QMLDIR} $<TARGET_FILE:${APP}> WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    
+    MESSAGE(STATUS ${CMAKE_BUILD_TYPE})
+    add_custom_command(TARGET ${APP} POST_BUILD
+            COMMAND "echo" "blah blah $<$<CONFIG:Debug>:${QTBIN}> $<$<NOT:$<CONFIG:Debug>>:Yooooo>")
+     
+    MESSAGE(STATUS "Added post-build call to windeployqt")
+endmacro ()
+
+# Macro to call macdeployqt after building
+macro (qt5_post_build_macdeployqt APP QMLDIR)
+
+    # Get bin directory of Qt from Qt5Core.dll
+    find_package(Qt5 REQUIRED Core)
+    get_target_property(Qt5_CoreLocation Qt5::Core LOCATION)
+    get_filename_component(QTBIN ${Qt5_CoreLocation} DIRECTORY)
+    MESSAGE(STATUS "\nQt bin: " ${QTBIN} "\n")
+    
+    add_custom_command(TARGET ${APP} POST_BUILD
+            COMMAND ${QTBIN}/macdeployqt.exe -qmldir ${QMLDIR} $<TARGET_FILE:${APP}> WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+     
+    MESSAGE(STATUS "Added post-build call to windeployqt")
+endmacro ()
+
+# Macro to copy Qt dlls on Windows
+macro (qt5_copy_dll APP DLL)
+    # find the release *.dll file
+    get_target_property(Qt5_${DLL}Location Qt5::${DLL} LOCATION)
+    # find the debug *d.dll file
+    get_target_property(Qt5_${DLL}LocationDebug Qt5::${DLL} IMPORTED_LOCATION_DEBUG)
+    
+    add_custom_command(TARGET ${APP} POST_BUILD
+       COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<CONFIG:Debug>:${Qt5_${DLL}LocationDebug}> $<$<NOT:$<CONFIG:Debug>>:${Qt5_${DLL}Location}> $<TARGET_FILE_DIR:${APP}>)
+endmacro ()
