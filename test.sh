af013e7f2dac78baf49c7e7af902f530f1277cbc
Jack McKernan <jmcker@outlook.com>
2018-07-09 01:29:43 -0500

    Clean lcov coverage on OSX

--- test.sh
+++ test.sh
@@ -38,5 +38,31 @@ echo
 lcov -c -d build -o build/coverage.actual
 lcov -a build/coverage.base -a build/coverage.actual -o build/coverage.info
 
+# Remove the separate reports
+rm build/coverage.base build/coverage.actual
+
+echo ""
+echo "Cleaning coverage report..."
+
+# Download script to strip mismarked function lines
+# Ref: https://github.com/linux-test-project/lcov/issues/30#issuecomment-353799085
+if [ ! -f ./remove-function-lines.py ]; then
+    curl https://raw.githubusercontent.com/symboxtra/lcov-llvm-function-mishit-filter/master/remove-function-lines.py > remove-function-lines.py && chmod +x remove-function-lines.py
+fi
+
+# Execute script if report was generated
+if [ -f build/coverage.info ]; then
+    python remove-function-lines.py build/coverage.info build/coverage.info
+    echo "Coverage report cleaned."
+fi
+
+echo ""
+if [ $error -eq 0 ]; then
+    echo -e "${YELLOW}BUILD SUCCESSFUL${NC}"
+else
+    echo -e "${RED}BUILD FAILED${NC}"
+fi
+echo ""
+
 # Exit with error code from tests
 exit $error
